5c7f3022e76477265b661b76826aaa1d239bcdad
Tarunkumarchelumalla <70788951+Tarunkumarchelumalla@users.noreply.github.com>
2023-06-30 11:43:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-store-implementation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jkrm5h)
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { reducers, metaReducers } from './reducers';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ]),
+    StoreModule.forRoot(reducers,{metaReducers})
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
